b49cbc57ac74cfa6d61c369be44826a5b16f5cad
Yoshihiro Tanaka <contact@cordea.jp>
2019-06-23 10:38:00 +0900

    enable auto increment

--- archivist.sql
+++ archivist.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS archivist;
 USE archivist;
 
 CREATE TABLE IF NOT EXISTS histories (
-    ID INT,
+    ID INT AUTO_INCREMENT,
     Command VARCHAR(3000) NOT NULL,
     Category VARCHAR(255) NOT NULL,
     PRIMARY KEY (ID)
